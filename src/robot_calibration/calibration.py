a9f1f4024a49087402dd1d551ec1b8aea6546b8a
Baptiste Busch <baptiste.busch@gmail.com>
2019-03-11 13:40:35 +0100

    Update code to calibrate a mobile base

--- src/robot_calibration/calibration.py
+++ src/robot_calibration/calibration.py
@@ -20,6 +20,7 @@ class Calibration(object):
         self.eef_frame = eef_frame
         self.camera_frame = camera_frame
         self.marker_frame = marker_frame
+        self.is_mobile = not base_marker_frame is None
         self.base_marker_frame = base_marker_frame
         self.bounds = self.init_bounds(abs_range_pos, abs_range_rot)
         self.tfl = tf.TransformListener(True, rospy.Duration(2))  # tf will have 2 seconds of cache
@@ -43,7 +44,7 @@ class Calibration(object):
     def record_calibration_points(self, continuous = True, duration=60, min_dist=0.01, max_dur=0.05):
         mat_robot = [] # Matrix of all calibration points of eef_frame in robot_frame
         mat_camera = [] # Matrix of all calibration points of marker_frame in frame camera_frame
-        mat_mobile_base = []
+        mat_mobile_base = [] # Matrix of all calibration points of marker_frame in frame camera_frame
         max_dur = rospy.Duration(max_dur) # seconds
         duration = rospy.Duration(duration)
         
@@ -65,20 +66,20 @@ class Calibration(object):
                         mat_robot.append(np.array(pose_rg_robot))
                         mat_camera.append(np.array(pose_rg_opt))
                         last_point = pose_rg_robot
-                        if not self.base_marker_frame is None:
+                        if self.is_mobile:
                             try:
-                                pose_mobile_base = self.tfl.lookupTransform(self.robot_frame, self.eef_frame, rospy.Time(0))
+                                pose_rg_base = self.tfl.lookupTransform(self.camera_frame, self.base_marker_frame, rospy.Time(0))
                             except Exception, e:
-                            print("Base <-> Camera transformation not available at the last known common time:", e.message)
+                                print("Base <-> Camera transformation not available at the last known common time:", e.message)
+                            else:
+                                mat_mobile_base.append(np.array(pose_rg_base))
                         print("pose recorded")
-            # else:
-            #     print "TFs are", (now - ref_time).to_sec(), "sec late"
             
             if continuous:
                 rospy.sleep(0.25)
             else:
                 entry = raw_input("Press enter to record a new point or q-enter to quit ({} points)".format(len(mat_robot)))
-        return mat_camera, mat_robot
+        return mat_camera, mat_robot, mat_mobile_base
 
     @staticmethod
     def extract_transforms(flat_transforms):
@@ -100,7 +101,7 @@ class Calibration(object):
         return [map(float, calibration_matrix[0]), map(float, calibration_matrix[1].tolist())]
 
     @staticmethod
-    def evaluate_calibration(calibrations, coords_robot, coords_opt):
+    def evaluate_calibration(calibrations, coords_robot, coords_opt, coords_base):
         def quaternion_cost(norm_coeff):
             C = 0
             for transform in list_calibr:
@@ -127,6 +128,9 @@ class Calibration(object):
             opt = coords_opt[i]
             product = transformations.multiply_transform(robot, B)
             product = transformations.multiply_transform(A, product)
+            if self.is_mobile:
+                mobile_base = coords_base[i]
+                product = transformations.multiply_transform(mobile_base, product)
             product[1] /= np.linalg.norm(product[1])
             cost += distance_cost(opt, product)
         return cost
@@ -134,20 +138,36 @@ class Calibration(object):
     def calibrate(self):
         raw_input("Press enter to start the recording process")
         # Record during 60 sec... set continuous=False for an interactive mode
-        mat_camera, mat_robot = self.record_calibration_points(continuous=True)
-
-        initial_guess = [0,0,0,0,0,0,1]*2
+        mat_camera, mat_robot, mat_mobile_base = self.record_calibration_points(continuous=True)
+        initial_guess = [0,0,0,0,0,0,1]*2 # have to estimate two full rotation matrices
         print("Recording finished, calibrating, please wait.")
         t0 = time.time()
         # Be patient, this cell can be long to execute...
-        result = minimize(self.evaluate_calibration, initial_guess, args=(mat_robot, mat_camera, ),
+        result = minimize(self.evaluate_calibration, initial_guess, args=(mat_robot, mat_camera, mat_mobile_base),
                           method='L-BFGS-B', bounds=self.bounds)
         print time.time()-t0, "seconds of optimization"
         result_list = self.extract_transforms(result.x)
 
         calibration_matrix_a = self.result_to_calibration_matrix(result_list[0])
+        calibration_matrix_b = self.result_to_calibration_matrix(result_list[1])
 
-        with open(join(self.conf_dir, "calibration_matrix.yaml"), 'w') as f:
-            yaml.dump(calibration_matrix_a, f)
+        if exists(join(self.conf_dir, "calibration_matrices.yaml")):
+            with open(join(self.conf_dir, "calibration_matrices.yaml"), 'r') as f:
+                calibrations = yaml.load(f)
+        else:
+            calibrations = {}
+        
+        if self.is_mobile:
+            key = self.base_marker_frame + "-" + self.robot_frame
+            calibrations[key] = calibration_matrix_a
+            key = self.eef_frame + "- " + self.marker_frame
+            calibrations[key] = calibration_matrix_b
+        else:
+            key = self.camera_frame + "-" + self.robot_frame
+            calibrations[key] = calibration_matrix_a
+            key = self.eef_frame + "-" + self.marker_frame
+            calibrations[key] = calibration_matrix_b
 
+        with open(join(self.conf_dir, "calibration_matrices.yaml"), 'w') as f:
+            yaml.dump(calibration, f)
         print("Calibration complete")
